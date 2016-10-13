d7d886c1b196015cb088238b067bd1481bb30889
Tim Christensen <mail@timchristensen.de>
2016-10-13 11:57:56 +0200

    Bilder automatisch verkleinern

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,9 @@
 class Photo < ActiveRecord::Base
   belongs_to :leg
-  has_attached_file :image
+  has_attached_file :image,
+    :styles => {
+      :medium => "560x840"
+    }
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
   validates_attachment_presence :image
 end
