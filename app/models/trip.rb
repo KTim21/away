71db013fb17e69d6d447b4e6cdcaf7555a941655
Tim Christensen <mail@timchristensen.de>
2016-10-13 17:06:40 +0200

    Größe Tour-Foto

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,7 +2,10 @@ class Trip < ActiveRecord::Base
   has_many :legs, -> { order(:date) }
   has_and_belongs_to_many :users, -> { uniq }, join_table: :usertrips
   
-  has_attached_file :image
+  has_attached_file :image,
+    :styles => {
+      :medium => "560x840"
+    }
   do_not_validate_attachment_file_type :image
   
   validates :title, presence: true
